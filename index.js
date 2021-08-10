1186f1e85faec57f24421efb14980bf9c64d3363
k-mill1 <kaisamill@gmail.com>
2021-08-10 16:38:35 +0100

    First version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class Room {
     this._description = "";
     this._linkedRooms = {};
     this._character = "";
+    this._item = "";
   }
 
   get name () {
@@ -15,7 +16,11 @@ class Room {
   }
 
   get character () {
-    return this._character
+    return this._character;
+  }
+
+  get item () {
+    return this._item;
   }
 
   set name (value) {
@@ -28,7 +33,7 @@ class Room {
 
   set description (value) {
     if (value.length < 4) {
-      alert('Name needs to be at least 4 characters');
+      alert('Description needs to be at least 4 characters');
       return;
     }
     this._description = value;
@@ -36,6 +41,10 @@ class Room {
 
   set character (value) {
     this._character = value;
+  }
+
+  set item (value) {
+    this._item = value;
   }
 
   describe () {
@@ -57,12 +66,140 @@ class Room {
 
   getDetails () {
     const entries = Object.entries(this._linkedRooms);
-    const details = [];
+    let details = []
     for (const [direction, room] of entries) {
-      const text = 'The ' + room._name + ' is to the ' + direction;
+      let text = " The " + room._name + " is to the " + direction;
       details.push(text);
     }
     return details;
+  }
+}
+
+class Character {
+  constructor(name) {
+    this._name = name;
+    this._description = "";
+    this._conversation = "";
+  }
+
+  set name (value) {
+    if (value.length < 4) {
+      alert('Name needs to be at least 4 characters');
+      return;
+    }
+    this._name = value;
+  }
+
+  set description (value) {
+    if (value.length < 4) {
+      alert('Decription needs to be at least 4 characters');
+      return;
+    }
+    this._description = value;
+  }
+
+  set conversation (value) {
+    if (value.length < 4) {
+      alert('Conversation needs to be at least 4 characters');
+      return;
+    }
+    this._conversation = value;
+  }
+  get name () {
+    return this._name;
+  }
+
+  get description () {
+    return this._description;
+  }
+
+  get conversation() {
+    return this._conversation;
+  }
+     
+  describe () {
+    return 'You meet ' + this._name + ' who is ' + this._description
+  }
+
+  converse() {
+    return this._name + " says " + "'" + this._conversation + "'";
+  }
+}     
+    
+class Enemy extends Character {
+  constructor (name, weakness) {
+    super(name);
+    this._weakness = weakness;
+    }
+     
+  set weakness (value) {
+    if (value.length < 4) {
+      alert('Decription needs to be at least 4 characters');
+      return;
+    }
+    this._weakness = value;
+  }
+
+  get weakness () {
+    return this._weakness;
+  }
+
+  fight (item) {
+    if (item == this._weakness) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Friend extends Character {
+  constructor (name, item) {
+    super(name);
+    this._item = item;
+    }
+     
+  set item (value) {
+    if (value.length < 4) {
+      alert('Decription needs to be at least 4 characters');
+      return;
+    }
+    this._item = value;
+  }
+}
+
+class Item {
+  constructor(name) {
+    this._name = name
+    this._description = ""
+  }
+
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name needs to be at least 4 characters");
+      return;
+    }
+    this._name = value;
+  }
+
+  set description(value) {
+    if (value.length < 4) {
+      alert("Decription needs to be at least 4 characters");
+      return;
+    }
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  describe() {
+    return 'This is a ' + this._name
   }
 }
 
@@ -76,12 +213,9 @@ const Lounge = new Room('lounge');
 Lounge.description = 'large room with a chandelier in the ceiling and a red velvet sofa next to a fireplace';
 const Bedroom = new Room('bedroom');
 Bedroom.description = 'spacious room with a large red canopy bed in the middle';
-const Closet = new Room('closet');
-Closet.description = 'small room with a large mirror in the front and piles of clothes everywhere';
 const Courtyard = new Room('courtyard');
 Courtyard.description = 'large green space with blossoming apple trees';
-const Cave = new Room('cave');
-Cave.description = 'dark creepy space with water dripping from the walls';
+
 
 Hallway.linkRoom('west', Kitchen);
 Hallway.linkRoom('east', Lounge);
@@ -92,117 +226,74 @@ Pantry.linkRoom('south', Kitchen);
 Lounge.linkRoom('west', Hallway);
 Lounge.linkRoom('north', Bedroom);
 Bedroom.linkRoom('south', Lounge);
-Bedroom.linkRoom('north', Closet);
-Closet.linkRoom('south', Bedroom);
 Courtyard.linkRoom('north', Cave);
 Courtyard.linkRoom('south', Hallway);
-Cave.linkRoom('south', Courtyard);
+
+
+const Steak = new Item('steak');
+const Sword = new Item('sword');
+Pantry.item = Sword;
+const Key = new Item('key');
+Bedroom.item = Key;
+
+const Lady = new Friend('Alice');
+Lady.conversation = 'Hello my friend';
+Lady.description = 'an old lady with kind eyes and long gray hair';
+Lady.item = Steak;
+Kitchen.character = Lady;
+
+const Dragon = new Enemy('Draco');
+Dragon.conversation = 'Zzzzzz';
+Dragon.description = 'a sleeping dragon';
+Dragon.weakness = 'sword';
+
+Courtyard.character = Dragon;
+
+const Dog = new Enemy('Beast');
+Dog.conversation = 'Grrrrr';
+Dog.description = 'an angry growling guard dog';
+Dog.weakness = 'steak';
+
+Lounge.character = Dog;
+
 
 function displayRoom (room) {
-  const content = room.describe();
-  document.getElementById('textarea').innerHTML = content;
+  let occupantMsg = "";
+  if (room.character == "") {
+    occupantMsg = ""
+  } else {
+    occupantMsg = room.character.describe() + ". " + room.character.converse()
+  }
+      
+  const textContent = room.describe() 
+  + "</p>" + "<p>" + occupantMsg + "</p>" + "<p>" + room.getDetails() + "</p>";
+        
+  document.getElementById('textarea').innerHTML = textContent;
   document.getElementById('ui').focus();
 }
- 
 
-      // class Character {
-      //   constructor (name, description, conversation) {
-      //     this._name = name;
-      //     this._description = description;
-      //     this._conversation = conversation;
-      //   }
-      
-      //   get name () {
-      //     return this._name
-      //   }
-      
-      //   get description () {
-      //     return this._description
-      //   }
-      
-      //   get conversation () {
-      //     return this._conversation
-      //   }
-      
-      //   set name (value) {
-      //     this._name = value
-      //   }
-      
-      //   set description (value) {
-      //     this._description = value
-      //   }
-      
-      //   set conversation (value) {
-      //     this._conversation = value
-      //   }
-      
-      //   describe () {
-      //     return 'In front of you is a ' + this._name + ' who is ' + this._description
-      //   }
-      
-      //   talk () {
-      //     return this._conversation
-      //   }
-      // }
-      
-      // class Enemy extends Character {
-      //   constructor (name, description, conversation, weakness) {
-      //     super(name, description, conversation);
-      //     this._weakness = weakness;
-      //   }
-      
-      //   fight (item) {
-      //     if (item == this._weakness) {
-      //       return true;
-      //     } else {
-      //       return false;
-      //     }
-      //   }
-      // }
-      
-      // const Troll = new Enemy('troll', 'hungry', 'ROAAAR!!', 'apple')
-      // console.log(Troll.fight('apple'))
-      
-      // class Item {
-      //   constructor (name, description) {
-      //     this._name = name;
-      //     this._description = description;
-      //   }
-      
-      //   get name () {
-      //     return this._name
-      //   }
-      
-      //   get description () {
-      //     return this._description
-      //   }
-      
-      //   set name (value) {
-      //     this._name = value
-      //   }
-      
-      //   set description (value) {
-      //     this._description = value
-      //   }
-      // }
-      
-      
-      function startGame () {
-        let currentRoom = Hallway;
+
+
+function startGame () {
+  let currentRoom = Hallway;
+  displayRoom(currentRoom);
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      const command = document.getElementById('ui').value;
+      const directions = ['north', 'south', 'east', 'west']
+      if (directions.includes(command.toLowerCase())) {
+        currentRoom = currentRoom.move(command.toLowerCase())
         displayRoom(currentRoom);
-      
-        document.addEventListener('keydown', function(event) {
-          if (event.key === 'Enter') {
-            const command = document.getElementById('ui').value;
-            const directions = ['north', 'south', 'east', 'west']
-            if (directions.includes(command.toLowerCase())) {
-              currentRoom = currentRoom.move(command.toLowerCase())
-              displayRoom(currentRoom);
-            } else {
-              document.getElementById('ui').value = ""
-              alert('that is not a valid command, please try again')
-            }
-          }
-        });
+      } else {
+        document.getElementById('ui').value = ""
+        alert('that is not a valid command, please try again')
       }
-      startGame()
+    }
+  });
+}
+
+startGame()
+
+
+
